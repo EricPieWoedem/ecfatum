5bc48f070ecc94d9e6cb6f75be660733a5a748bc
EricPieWoedem <coco.pie3030@gmail.com>
2025-03-07 11:25:43 +0000

    updated contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,7 +36,7 @@ export const Contact = () => {
     setResponse(null);
 
     try {
-      const res = await fetch("http://localhost:3000/api/contact", {
+      const res = await fetch("https://server-phi-eight-38.vercel.app//api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
